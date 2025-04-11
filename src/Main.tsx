0df0c17d181865c903007c0898de5036dc0beba6
Atchutha Rao T A <atchutha57@gmail.com>
2025-04-11 15:12:26 +0530

    Refactor App component to integrate routing directly and remove unnecessary imports

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,6 +1,32 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Layout from "./components/Layout";
+import Profile from "./pages/profile/Profile";
+import About from "./pages/about/About";
+import WorkExperience from "./pages/workexperience/WorkExperience";
+ import Projects from './pages/projects/Projects';
+import Interests from './pages/interests/Interests';
+import Education from './pages/education/Education';
+import Contact from './pages/contact/Contact';
+
+export default function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/work-experience" element={<WorkExperience />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/interests" element={<Interests />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
